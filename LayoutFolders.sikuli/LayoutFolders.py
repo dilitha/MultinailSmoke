f61a9dc2516d26f1c9430efa9c11beb982defcc8
Dilitha Vithanage <dilitha.vithanage@multinail.com>
2022-07-08 11:25:46 +1000

    Add LayoutFolders.sikuli

---
+++ LayoutFolders.sikuli/LayoutFolders.py
@@ -0,0 +1,13 @@
+from sikuli import *
+
+CustomisedLayouts = {
+    'layout1': 'LayoutType_1\\\Layout',
+    'layout2': 'LayoutType_2\\\Layout',
+    'layout3': 'LayoutType_3\\\Layout',
+}
+
+LayoutMapping = {
+    'customised-1': 'Layout description1',
+    'customised-2': 'Layout description2',
+    'customised-3': 'Layout description3',
+}
