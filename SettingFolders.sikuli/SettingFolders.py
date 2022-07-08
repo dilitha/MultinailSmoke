cd68c55d8aac071df71fcb72f00d746aaaf695ca
Dilitha Vithanage <dilitha.vithanage@multinail.com>
2022-07-08 11:31:38 +1000

    Add SettingFolders.sikuli

---
+++ SettingFolders.sikuli/SettingFolders.py
@@ -0,0 +1,13 @@
+from sikuli import *
+
+CustomisedSettings = {
+    'setting1': 'SettingsType_1\\\Settings',
+    'setting2': 'SettingsType_2\\\Settings',
+    'setting3': 'SettingsType_3\\\Settings',
+}
+
+SettingsMapping = {
+    'customised-1': 'Settings description1',
+    'customised-2': 'Settings description2',
+    'customised-3': 'Settings description3',
+}
